8abfbb536a6841fcc371f628999b6f38856a7e26
Simon Florez <sflorezs1@eafit.edu.co>
2024-02-15 23:42:49 -0500

    init project

---
+++ apps/dev-transactions-fe-monorepo/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="dev-transactions-fe-monorepo" />
+    </div>
+  );
+}
+
+export default App;
