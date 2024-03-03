c88c3234d1cb6dd819501f65dd943e4660ef986d
vmunozm <victord@VictorDanielM>
2024-03-03 17:14:46 -0500

    :sparkles: Adding fields configurations

--- apps/dev-transactions-fe-monorepo/src/app/header.tsx
+++ apps/dev-transactions-fe-monorepo/src/app/header.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styles from './app.module.scss';
-import Logo from './login/devtran-logo.ico'
+import Logo from './login/devtran-logo.ico';
 
 const Header = () => {
   return (
-    <Navbar bg="transparent" expand="lg" className={`${styles.headerBackground}`}>
+    <Navbar bg="transparent" expand="lg" className={styles.headerBackground}>
       <Navbar.Brand href="#home" className={styles.logo}>
         <img src={Logo} alt="Logo" className={styles.logo} />
-      </Navbar.Brand >
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="">
-          <Nav.Link  as={Link} to="/download" className={styles.navLink}>Download</Nav.Link>
-          <Nav.Link as={Link} to="/edit" className={styles.navLink}>Edit</Nav.Link>
-          <Nav.Link as={Link} to="/transfer" className={styles.navLink}>Transfer Operator</Nav.Link>
+      <Navbar.Collapse id="basic-navbar-nav" className={styles.headerNavRight}>
+        <Nav>
+          <Nav.Link as={Link} to="/download" className={styles.navLink}>Download</Nav.Link>
+          <Nav.Link as={Link} to="/edit" className={styles.navLink}>Upload</Nav.Link>
+          <NavDropdown title="Request" id="basic-nav-dropdown" className={styles.navDropdown}>
+            <NavDropdown.Item href="#action/3.1">Change Operator</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Request document</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Autenticate document</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">University Document</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link as={Link} to="/transfer" className={styles.navLink}>Approvals</Nav.Link>
+          <Nav.Link as={Link} to="/transfer" className={styles.navLink}>Group</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
